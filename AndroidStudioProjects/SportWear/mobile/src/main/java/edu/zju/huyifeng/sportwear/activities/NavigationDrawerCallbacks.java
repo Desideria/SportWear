32378aedf2d289b8b24916302a6c6ee6f49b9a78
Oohcar <rachoo8906@gmail.com>
2015-03-12 22:14:31 +0800

    delete old files

--- AndroidStudioProjects/SportWear/mobile/src/main/java/edu/zju/huyifeng/sportwear/activities/NavigationDrawerCallbacks.java
+++
@@ -1,8 +0,0 @@
-package edu.zju.huyifeng.sportwear.activities;
-
-/**
- * Created by huyifeng on 15/3/9.
- */
-public interface NavigationDrawerCallbacks {
-    void onNavigationDrawerItemSelected(int position);
-}
